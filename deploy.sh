26d281a1221fd3501fadd2903f6daf7dd7e712ea
Marcos <marbaez@gmail.com>
2023-10-06 11:12:49 +0200

    artifact sharing

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 FILE=build/object.txt
 echo "Deploy begins"
 
+mkdir -p build
+buildkite-agent artifact download build/object.txt build/object.txt
+
 if test -f "${FILE}"; then
     echo "${FILE} exists"
 else
